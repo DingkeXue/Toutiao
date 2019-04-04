9e6c5a0b5226b1cd5887204c4c12d4ac3e34309b
Xue <605534423@qq.com>
2019-04-04 18:06:16 +0800

    '今日头条'

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$('.navbar li').click(function(){
+    $('.navbar li').removeClass('active');
+    $(this).addClass('active');
+});
+
+$('.side-bar li').click(function(){
+    $('.side-bar li').removeClass('active');
+    $(this).addClass('active');
+});
